06156b881e494ea0a08e539607555de85fc21b5c
dsungc1111 <eotjd9808@naver.com>
2024-06-19 14:19:52 +0900

    Initial Commit

---
+++ WeatherProj/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherProj
+//
+//  Created by 최대성 on 6/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .orange
+    }
+
+
+}
+
---
+++ WeatherProj/WeatherInfo.swift
@@ -0,0 +1,11 @@
+//
+//  WeatherInfo.swift
+//  WeatherProj
+//
+//  Created by 최대성 on 6/19/24.
+//
+
+import Foundation
+
+
+
