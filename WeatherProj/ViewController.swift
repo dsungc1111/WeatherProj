5490569c21c6b3a43b32a3bbe61285ebc28ae6fb
dsungc1111 <eotjd9808@naver.com>
2024-06-19 14:52:48 +0900

    - JSON형태 기준의 데이터 생성 및 디코딩여부 확인

--- WeatherProj/ViewController.swift
+++ WeatherProj/ViewController.swift
@@ -6,12 +6,39 @@
 //
 
 import UIKit
+import Alamofire
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .orange
+        view.backgroundColor = .white
+        callWeather()
+    }
+    
+    
+    func callWeather() {
+        
+        let url = "https://api.openweathermap.org/data/2.5/weather?"
+        
+        let param: Parameters = [
+            "APIKey" : APIKey().weatherKey,
+            "lat" : "37.5166",
+            "lon" : "126.8915",
+            "units" : "metric",
+            "lang" : "kr"
+        ]
+        
+        AF.request(url, parameters: param).responseString { response in
+            switch response.result {
+            case .success(let value):
+                print(value)
+            case .failure(let error):
+                print(error)
+            }
+        }
+        
     }
 
 
--- WeatherProj/WeatherInfo.swift
+++ WeatherProj/WeatherInfo.swift
@@ -7,5 +7,49 @@
 
 import Foundation
 
+struct Sys {
+    let type, id, sunrise, sunset: Int
+    let country: String
+    
+}
+
+struct CloudsInfo {
+    let all: Int
+}
+
+struct WindInfo {
+    let speed: Double
+    let deg: Int
+    let gust: Double
+}
+
+struct WeatherInfo {
+    let temp: Double
+    let feels_like: Double
+    let temp_min: Double
+    let temp_max: Double
+    let pressure: Int
+    let humidity: Int
+    
+}
+
+struct TodayWeather {
+    let id: Int
+    let main: String
+    let description: String
+    let icon: String
+}
 
 
+struct Weather {
+    let coord: [Int]
+    let weather : [TodayWeather]
+    let base: String
+    let main: [WeatherInfo]
+    let visibility: Int
+    let wind: [WindInfo]
+    let clouds: [CloudsInfo]
+    let dt: Int
+    let sys: [Sys]
+    let name: String
+}
